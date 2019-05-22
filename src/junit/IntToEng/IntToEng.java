e4014675b7b2dfae048a3536a5f2896082566a4d
moemoekyun1201 <g17911hm@gm.tsuda.ac.jp>
2019-05-22 12:24:52 +0900

    last

--- src/junit/IntToEng/IntToEng.java
+++ src/junit/IntToEng/IntToEng.java
@@ -1,65 +1,63 @@
 	package junit.IntToEng;
-    import java.util.Scanner;
-	
-	public class IntToEng {
-		// メインメソッド
-	
-	  /* public static void main
-	    (String[] args) {
-	    	for(;;){
-	    	System.out.print("入力：");
-	        Scanner sc = new Scanner(System.in);
-	
-	        int input = sc.nextInt();
-	       
-	        System.out.print("出力：");
-	        System.out.println(translateEng(input));
-	    	}
-	    }*/
+
+public class IntToEng {
+	static String[] a={"  ","one","two","three","four","five","six","seven","eight","nine","ten",
+			"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+	static String[] b={"twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
+
 	
 	    // 数値を英訳する変換するメソッド
 	
 	    static String translateEng(int n) {
+	    	if(n==0) return "zero";
+	    	if(n>0&&n<=99){
+	    		return doubleDigit(n);
+	    	}
+	    	if(n>=100 && n<=999){
+	    		return threeDigit(n);
+	    	}
+	    	if(n>=1000&&10000>n){
+	    		return fourDigit(n);
+	    	}
+	    	return " ";
 
-	    	String[] a={"zero","one","two","three","four","five","six","seven","eight","nine","ten",
-	    			"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
-	    	String[] b={"twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
-	    	String c="hundred";
-	    	
-	if(n<=19){
+	    	 }
+	    
+	 static String doubleDigit(int n){//２桁のとき
+	
+	   if(n<=19){
 	    	return a[n];
-	}
-	if(n>=20&&n<=99){
-		String n1=String.valueOf(n);
-		int x1 = Integer.parseInt(n1.substring(0, 1));
-		int x2 = Integer.parseInt(n1.substring(1, 2));
-		if(x2==0){
-			return b[x1-2];
-		}
+	   }
+	   if(n>=20&&n<=99){
+		    String n1=String.valueOf(n);
+		    int x1 = Integer.parseInt(n1.substring(0, 1));
+		    int x2 = Integer.parseInt(n1.substring(1, 2));
+		    if(x2==0){
+			return b[x1-2]; 
+		   }
 	        return b[x1-2]+" "+a[x2];
-	}if(100<=n&&n<=999){
-		String n1=String.valueOf(n);
-		int x1 = Integer.parseInt(n1.substring(0, 1));
-		int x2 = Integer.parseInt(n1.substring(1, 2));
-		int x3 = Integer.parseInt(n1.substring(2, 3));
-		if(x2==0&&x3==0){
-			return a[x1]+" " +c;
-		}
-		if(x2==0){
-			return a[x1]+" "+c+" "+a[x3];
-		}
-		if(x2==1){
-			if(x3==0){
-				return a[x1]+" "+c+" "+a[10];
-			}
-			int n2 = Integer.parseInt(n1.substring(1, 3));
-			return a[x1]+" "+c+" "+a[n2];
-		}
-		if(x3==0){
-			return a[x1]+" "+c+" "+b[x2-2];
-		}
-		return a[x1]+" "+c+" "+b[x2-2]+" "+a[x3];
-	}
-	 return " ";   
+	     }
+	   return " ";
+        }
+	    
+	    static String threeDigit(int n){//３桁のとき
+	  
+	    	String n1=String.valueOf(n);
+		    int x1 = Integer.parseInt(n1.substring(0, 1));
+		    
+		    return a[x1]+" hundred "+doubleDigit(Integer.parseInt(n1.substring(1, 3)));
+		
+			    }
+	    static String fourDigit(int n){
+	    	String n1=String.valueOf(n);
+		    int x1 = Integer.parseInt(n1.substring(0, 1));
+		    int x2 = Integer.parseInt(n1.substring(1, 2));
+		    if(x2==0){
+				return a[x1]+" thousand "+doubleDigit(Integer.parseInt(n1.substring(2, 4))); 
+			   }
+		    
+		    return a[x1]+" thousand "+threeDigit(Integer.parseInt(n1.substring(1, 4)));
 	    }
 	}
+
+
